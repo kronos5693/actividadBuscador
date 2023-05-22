e8d1d8a099b2519c42f768c28d4b58771fd34874
Alejandro <w.krull@hotmail.com>
2023-05-22 19:29:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# actividadBuscador
+ 
---
+++ src/productos/productos.interfaces.ts
@@ -0,0 +1,8 @@
+// exportar y crear la interfaces para producto la misma
+// se tiene que utilizar en el archivo productos.ts
+export interface producto {
+    id: number,
+    nombre: string,
+    precio: number,
+	
+}
